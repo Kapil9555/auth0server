041cc2f057256911cdbc193974a163da57ea3374
kapil9555 <kapilbehwal9555@gmail.com>
2025-03-31 12:58:06 +0530

    final3

--- server.js
+++ server.js
@@ -4,13 +4,26 @@ import cors from 'cors';
 import authRouter from './routes/authRouter.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+// Routes
+app.get('/', async (req, res) => {
     res.status(200).json({ message: "Server is running" });
 });
 
 app.use('/auth', authRouter);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
